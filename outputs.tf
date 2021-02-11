5b98edd74631b95a4e94422fc3aa3a65d827cb6c
Kyle Smith <kyle.r.smith@accenture.com>
2021-02-11 12:49:44 -0500

    Common#512 run terraform 0.12upgrade on repo

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
 # VPC
 output "efs_id" {
   description = "The ID of the EFS"
-  value       = "${aws_efs_file_system.this.id}"
+  value       = aws_efs_file_system.this.id
 }
 
 output "kms_key_id" {
   description = "The ARN for the KMS encryption key"
-  value       = "${aws_efs_file_system.this.kms_key_id}"
+  value       = aws_efs_file_system.this.kms_key_id
 }
 
 output "dns_name" {
   description = "The DNS name for the filesystem"
-  value       = "${aws_efs_file_system.this.dns_name}"
+  value       = aws_efs_file_system.this.dns_name
 }
+
